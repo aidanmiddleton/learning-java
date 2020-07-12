2599df5e8ab2c413a439cd66a4ac025157a7c17b
Aidan Middleton <aidanemiddleton@gmail.com>
2020-07-12 16:07:39 -0700

    added basic printing and tracking of grid layout

--- titac/TicTacToe/src/app/App.java
+++ titac/TicTacToe/src/app/App.java
@@ -1,7 +1,35 @@
 package app;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        
+        char [] [] gameBoard = {
+            {' ', '|', ' ', '|', ' '}, 
+            {'-', '+', '-', '+', '-'},  
+            {' ', '|', ' ', '|', ' '}, 
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '}, 
+        
+        };
+
+        printGameBoard(gameBoard);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your placement (1-9):");
+        int position = scan.nextInt();
+
+        System.out.println(position);
+
+    }
+
+    public static void printGameBoard(char [] [] gameBoard) {
+        for(char [] row : gameBoard) {
+
+            for(char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
     }
 }
